cd318488903a46ce2e1713f8bb4d0c31effc9535
petitviolet <violethero0820@gmail.com>
2015-11-08 23:04:32 +0900

    implemented State monad

---
+++ monad/src/main/java/net/petitviolet/monad/state/State.java
@@ -0,0 +1,116 @@
+package net.petitviolet.monad.state;
+
+import net.petitviolet.monad.Tuple;
+import net.petitviolet.monad.func.Function;
+
+/**
+ * State Monad
+ * @param <S>
+ * @param <A>
+ */
+public class State<S, A> {
+    private static final String TAG = State.class.getSimpleName();
+    // get current state, then return new value and state
+    final Function.F1<S, Tuple<A, S>> runState;
+
+    /**
+     * use unit or init Factory
+     * @param runState
+     */
+    private State(Function.F1<S, Tuple<A, S>> runState) {
+        this.runState = runState;
+    }
+
+    /**
+     * unit factory given by first value
+     * @param a
+     * @param <S>
+     * @param <A>
+     * @return
+     */
+    public static <S, A> State<S, A> unit(final A a) {
+        return State.init(new Function.F1<S, Tuple<A, S>>() {
+            @Override
+            public Tuple<A, S> invoke(S s) {
+                return Tuple.of(a, s);
+            }
+        });
+    }
+
+    /**
+     * init factory with runState function
+     * @param runState
+     * @param <S>
+     * @param <A>
+     * @return
+     */
+    public static <S, A> State<S, A> init(Function.F1<S, Tuple<A, S>> runState) {
+        return new State<>(runState);
+    }
+
+    /**
+     * put new state
+     * @param newState
+     * @return
+     */
+    public State<S, Void> put(final S newState) {
+        return State.init(new Function.F1<S, Tuple<Void, S>>() {
+            @Override
+            public Tuple<Void, S> invoke(S s) {
+                return Tuple.of(null, newState);
+            }
+        });
+    }
+
+    public Tuple<A, S> apply(S state) {
+        return runState.invoke(state);
+    }
+
+    /**
+     * get current value
+     * @return
+     */
+    public State<S, S> get() {
+        return State.init(new Function.F1<S, Tuple<S, S>>() {
+            @Override
+            public Tuple<S, S> invoke(S s) {
+                return Tuple.of(s, s);
+            }
+        });
+    }
+
+    /**
+     * monadic
+     * @param f
+     * @param <B>
+     * @return
+     */
+    public <B> State<S, B> flatMap(final Function.F1<? super A, State<S, B>> f) {
+        Function.F1<S, Tuple<B, S>> fb = new Function.F1<S, Tuple<B, S>>() {
+            @Override
+            public Tuple<B, S> invoke(S s) {
+                Tuple<A, S> as = runState.invoke(s);
+                State<S, ? extends B> sb = f.invoke(as.fst);
+                Tuple<B, S> bs = (Tuple<B, S>) sb.runState.invoke(as.snd);
+                return bs;
+            }
+        };
+        return State.init(fb);
+    }
+
+    /**
+     * map function to inner value
+     * @param f
+     * @param <B>
+     * @return
+     */
+    public <B> State<S, B> map(final Function.F1<? super A, ? extends B> f) {
+        return flatMap(new Function.F1<A, State<S, B>>() {
+            @Override
+            public State<S, B> invoke(A a) {
+                return State.unit(f.invoke(a));
+            }
+        });
+    }
+
+}
---
+++ monad/src/test/java/net/petitviolet/state/StateTest.java
@@ -0,0 +1,122 @@
+package net.petitviolet.monad.state;
+
+import net.petitviolet.monad.Tuple;
+import net.petitviolet.monad.func.Function;
+import net.petitviolet.monad.state.State;
+
+import org.junit.Test;
+
+import java.lang.Integer;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test cases for State monad
+ */
+public class StateTest {
+    @Test
+    public void testInit() {
+        System.out.println("testUnit");
+        Tuple<Integer, Integer> result = State.<Integer, Integer>init(new Function.F1<Integer, Tuple<Integer, Integer>>() {
+            @Override
+            public Tuple<Integer, Integer> invoke(Integer integer) {
+                return Tuple.of(1, integer);
+            }
+        }).apply(100);
+        System.out.println("testInit");
+        System.out.println(result);
+        assert result.fst == 1;
+        assert result.snd == 100;
+    }
+
+    @Test
+    public void testUnit() {
+        Tuple<Integer, Integer> result = State.<Integer, Integer>unit(1).apply(100);
+        System.out.println(result);
+        assert result.fst == 1;
+        assert result.snd == 100;
+    }
+
+    @Test
+    public void testMap() {
+        System.out.println("testMap");
+        Tuple<Integer, Integer> result = State.<Integer, Integer>unit(1)
+                .map(new Function.F1<Integer, Integer>() {
+                    @Override
+                    public Integer invoke(Integer integer) {
+                        return integer * 2;
+                    }
+                }).apply(100);
+        System.out.println(result);
+        assert result.fst == 2;
+        assert result.snd == 100;
+    }
+
+    @Test
+    public void testFlatMap() {
+        System.out.println("testFlatMap");
+        Tuple<Integer, Integer> result = State.<Integer, Integer>unit(1)
+                .flatMap(new Function.F1<Integer, State<Integer, Integer>>() {
+                    @Override
+                    public State<Integer, Integer> invoke(Integer integer) {
+                        return State.unit(integer * 2);
+                    }
+                }).apply(100);
+        System.out.println(result);
+        assert result.fst == 2;
+        assert result.snd == 100;
+    }
+
+    @Test
+    public void testCombined() {
+        System.out.println("testCombined");
+
+        Tuple<Integer, List<String>> result = State.init(new Function.F1<List<String>, Tuple<Integer, List<String>>>() {
+            @Override
+            public Tuple<Integer, List<String>> invoke(List<String> strings) {
+                strings.add("nice");
+                return Tuple.of(1, strings);
+            }
+        }).map(new Function.F1<Integer, Integer>() {
+            @Override
+            public Integer invoke(Integer i) {
+                System.out.println("map: " + i);
+                return i + 10;
+            }
+        }).map(new Function.F1<Integer, Integer>() {
+            @Override
+            public Integer invoke(Integer i) {
+                System.out.println("map: " + i);
+                return i * 2;
+            }
+        }).flatMap(new Function.F1<Integer, State<List<String>, Integer>>() {
+            @Override
+            public State<List<String>, Integer> invoke(final Integer integer) {
+                return State.init(new F1<List<String>, Tuple<Integer, List<String>>>() {
+                    @Override
+                    public Tuple<Integer, List<String>> invoke(List<String> strings) {
+                        if (integer % 2 == 0) {
+                            strings.add("awesome");
+                            return Tuple.of(integer, strings);
+                        } else {
+                            strings.add("Oops");
+                            return Tuple.of(-100, strings);
+                        }
+                    }
+                });
+            }
+        }).apply(new ArrayList<String>());
+
+        System.out.println(result);
+
+        assert result._1 == 22;
+        assert result._2.size() == 2;
+        assert result._2.get(0).equals("nice");
+        assert result._2.get(1).equals("awesome");
+    }
+}

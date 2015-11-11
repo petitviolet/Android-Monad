e9ad870328e974079f168d36d8f129b9f141dbad
petitviolet <violethero0820@gmail.com>
2015-11-11 11:18:43 +0900

    ListM directly implements List interface by holding list field

--- monad/src/main/java/net/petitviolet/monad/list/ListM.java
+++ monad/src/main/java/net/petitviolet/monad/list/ListM.java
@@ -7,10 +7,34 @@ import net.petitviolet.monad.func.Function;
 import net.petitviolet.monad.maybe.Maybe;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
-abstract public class ListM<A> extends ArrayList<A> {
+public class ListM<A> implements List<A> {
+    private final List<A> mList;
+
+    private ListM(List<A> mList) {
+        this.mList = mList;
+    }
+
+    private ListM() {
+        this.mList = new ArrayList<>();
+    }
+
+    public static <A> ListM<A> unit() {
+        return new ListM<>();
+    }
+
+    @SafeVarargs
+    public static <A> ListM<A> of(A... args) {
+        ListM<A> listM = ListM.unit();
+        Collections.addAll(listM, args);
+        return listM;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof ListM) || ((ListM) o).size() != this.size()) {
@@ -24,15 +48,99 @@ abstract public class ListM<A> extends ArrayList<A> {
         return true;
     }
 
+    @Override
+    public A get(int location) {
+        return mList.get(location);
+    }
+
+    @Override
+    public int hashCode() {
+        return mList.hashCode();
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        return mList.indexOf(object);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return mList.isEmpty();
+    }
+
+    @NonNull
+    @Override
+    public Iterator<A> iterator() {
+        return mList.iterator();
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        return mList.lastIndexOf(object);
+    }
+
+    @Override
+    public ListIterator<A> listIterator() {
+        return mList.listIterator();
+    }
+
+    @NonNull
+    @Override
+    public ListIterator<A> listIterator(int location) {
+        return mList.listIterator(location);
+    }
+
+    @Override
+    public A remove(int location) {
+        return mList.remove(location);
+    }
+
+    @Override
+    public boolean remove(Object object) {
+        return mList.remove(object);
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        return mList.removeAll(collection);
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        return mList.retainAll(collection);
+    }
+
+    @Override
+    public A set(int location, A object) {
+        return mList.set(location, object);
+    }
+
+    @Override
+    public int size() {
+        return mList.size();
+    }
+
     @NonNull
     @Override
     public ListM<A> subList(int start, int end) {
-        List<A> sublist = super.subList(start, end);
+        List<A> sublist = mList.subList(start, end);
         ListM<A> listM = ListM.unit();
         for (A item : sublist) {
             listM.add(item);
         }
         return listM;
+    }
+
+    @NonNull
+    @Override
+    public Object[] toArray() {
+        return mList.toArray();
+    }
+
+    @NonNull
+    @Override
+    public <T> T[] toArray(T[] array) {
+        return mList.toArray(array);
     }
 
 
@@ -47,31 +155,113 @@ abstract public class ListM<A> extends ArrayList<A> {
         return maybeGet(index).getOrElse(defaultValue);
     }
 
-    public abstract <B> B foldLeft(B acc, Function.F2<? super B, ? super A, ? super B> func);
-
-    public abstract <B> B foldRight(Function.F2<? super A, ? super B, ? super B> func, B acc);
-
-    public abstract <B> B foldMap(B acc, Function.F1<? super A, ? extends B> converter,
-                         Function.F2<? super B, ? super B, ? extends B> accumulator);
-
-    public abstract void foreach(Function.F<? super A> func);
-
-    public abstract <B> ListM<B> map(Function.F1<? super A, ? extends B> func);
-
-    public abstract <B> ListM<B> flatMap(Function.F1<? super A, ? extends ListM<B>> func);
-
-    public abstract ListM<A> filter(final Function.F1<? super A, Boolean> func);
-
-    public static <A> ListM<A> unit() {
-        return new ArrayListM<>();
+    public <B> B foldLeft(B acc, Function.F2<? super B, ? super A, ? super B> func) {
+        for (A item : this) {
+            acc = (B) func.invoke(acc, item);
+        }
+        return acc;
     }
 
-    public static <A> ListM<A> of(A... args) {
-        ListM<A> listM = ListM.unit();
-        Collections.addAll(listM, args);
-        return listM;
+    public <B> B foldRight(Function.F2<? super A, ? super B, ? super B> func, B acc) {
+        for (A item : this) {
+            acc = (B) func.invoke(item, acc);
+        }
+        return acc;
     }
 
-    public abstract Tuple<ListM<A>, ListM<A>> partition(Function.F1<? super A, Boolean> func);
+    public <B> B foldMap(B acc, Function.F1<? super A, ? extends B> converter,
+                         Function.F2<? super B, ? super B, ? extends B> accumulator) {
+        B result = acc;
+        for (A item : this) {
+            result = accumulator.invoke(result, converter.invoke(item));
+        }
+        return result;
+    }
 
+    public void foreach(Function.F<? super A> func) {
+        for (A item : this) {
+            func.invoke(item);
+        }
+    }
+
+    public <B> ListM<B> map(Function.F1<? super A, ? extends B> func) {
+        ListM<B> result = new ListM<>();
+        for (A item : this) {
+            result.add(func.invoke(item));
+        }
+        return result;
+    }
+
+    public <B> ListM<B> flatMap(Function.F1<? super A, ? extends ListM<B>> func) {
+        // use flatMap is not effective from the viewpoint of performance
+//        return flatMap(new Function.F1<A, ListM<B>>() {
+//            @SuppressWarnings("unchecked")
+//            @Override
+//            public ListM<B> invoke(A a) {
+//                return ListM.of(func.invoke(a));
+//            }
+//        });
+        ListM<B> result = new ListM<>();
+        for (A item : this) {
+            result.addAll(func.invoke(item));
+        }
+        return result;
+    }
+
+    public ListM<A> filter(Function.F1<? super A, Boolean> func) {
+        ListM<A> result = new ListM<>();
+        for (A item : this) {
+            if (func.invoke(item)) {
+                result.add(item);
+            }
+        }
+        return result;
+    }
+
+    public Tuple<ListM<A>, ListM<A>> partition(final Function.F1<? super A, Boolean> func) {
+        ListM<A> fst = this.filter(func);
+        ListM<A> snd = this.filter(new Function.F1<A, Boolean>() {
+            @Override
+            public Boolean invoke(A a) {
+                return !func.invoke(a);
+            }
+        });
+
+        return Tuple.of(fst, snd);
+    }
+
+    @Override
+    public void add(int location, A object) {
+        mList.add(location, object);
+    }
+
+    @Override
+    public boolean add(A object) {
+        return mList.add(object);
+    }
+
+    @Override
+    public boolean addAll(int location, Collection<? extends A> collection) {
+        return mList.addAll(location, collection);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends A> collection) {
+        return mList.addAll(collection);
+    }
+
+    @Override
+    public void clear() {
+        mList.clear();
+    }
+
+    @Override
+    public boolean contains(Object object) {
+        return mList.contains(object);
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        return mList.containsAll(collection);
+    }
 }
